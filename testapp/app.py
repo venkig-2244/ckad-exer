4f2918ec5aaf48f9ac6bc2d8c75303ade2a52225
Venkatesh G <itsvenkatesh@gmail.com>
2020-01-08 22:22:49 +0530

    flask app

--- testapp/app.py
+++ testapp/app.py
@@ -1,12 +1,14 @@
 #!/usr/local/bin/python
 from flask import Flask
+import logging as logger
+logger.basicConfig(level="DEBUG")
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return ('<h1>Home Page</h1')
+    return ('<h1>Welcome to Kubernetes Tutorial</h1')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
+    app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=True)
