c0c14c79f169f183712e2c016f931cef30e55db3
Venkatesh G <itsvenkatesh@gmail.com>
2020-01-09 22:42:47 +0530

    added template and dynamic html page

--- testapp/app.py
+++ testapp/app.py
@@ -1,14 +1,33 @@
 #!/usr/local/bin/python
-from flask import Flask
+from flask import Flask, render_template
 import logging as logger
 logger.basicConfig(level="DEBUG")
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Venkatesh G',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 01, 2020'
+    },
+    {
+        'author': 'Vibhas V G',
+        'title': 'Table Tennis',
+        'content': 'All about Playing Table Tennis',
+        'date_posted': 'January 08, 2020'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return ('<h1>Welcome to Kubernetes Tutorial</h1')
+    return render_template("home.html", posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template("about.html", title="About")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=True)
