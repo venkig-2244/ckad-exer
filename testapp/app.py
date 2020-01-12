5a1a8c235cf6f12eb4c5312aa816073b63942333
Venkatesh G <itsvenkatesh@gmail.com>
2020-01-12 14:55:58 +0530

    added forms

--- testapp/app.py
+++ testapp/app.py
@@ -1,9 +1,22 @@
 #!/usr/local/bin/python
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 import logging as logger
+from forms import RegistrationForm, LoginForm
+
 logger.basicConfig(level="DEBUG")
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'f57fc334364d5462924988fd555fd60a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Vibhas123@localhost/appserver'
+
+db = SQLAlchemy(app)
+
+class Example(db.Model):
+    __tablename__ = 'emp_details'
+    id = db.Column('id', db.Integer, primary_key = True)
+    name = db.Column('name', db.Unicode)
 
 posts = [
     {
@@ -24,6 +37,20 @@ posts = [
 @app.route('/home')
 def home():
     return render_template("home.html", posts=posts)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        redirect(url_for('home'))
+
+    return render_template("register.html", title="Register", form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 @app.route('/about')
 def about():
